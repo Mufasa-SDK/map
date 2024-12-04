c9d3da3730342f989da43ae7a99e91f7afe607fa
Davy <davykanningstudent@gmail.com>
2024-12-04 08:32:58 +0100

    Revert "Change chunk parsing, and fix path generation Y coordinates"
    This reverts commit de42295a4fad516702d6b7e91bbcdc93e621563b.

--- js/main/main_osrs.js
+++ js/main/main_osrs.js
@@ -319,12 +319,9 @@ void function (global) {
             if (this.pathPoints.length > 0) {
                 let pathText = 'Tile[] path = new Tile[] {\n';
                 this.pathPoints.forEach((point, index) => {
-                    // Convert the map coordinates to Mufasa-style coordinates
-                    let convertedCoords = this._convertMapCoords(point.lat, point.lng);
-
-                    pathText += `\tnew Tile(${convertedCoords})`;
+                    pathText += `\tnew Tile(${point.coords})`;
                     if (index < this.pathPoints.length - 1) {
-                        pathText += ',\n'; // Add a comma between coordinates, except the last one
+                        pathText += ',\n';  // Add a comma between coordinates, except the last one
                     }
                 });
                 pathText += '\n};';
@@ -508,7 +505,7 @@ void function (global) {
             }
 
             chunkData += chunkStrings.join(',\n') + '\n}, "';
-            chunkData += Array.from(planesSet).sort().join('"-"') + '");\n';
+            chunkData += Array.from(planesSet).sort().join('", "') + '");\n';
 
             this._textbox.innerHTML = `<pre>${chunkData}</pre>`;
         },
